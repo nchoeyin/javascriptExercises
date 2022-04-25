c7d4607a34ba4c2ed5935d31a9584ba671bfb68e
Ngawang C <ngawangchoeying303@gmail.com>
2022-04-25 18:21:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+// Problem
+//Select all elements that have the class od "done" and save them in a variable called doneTodos
+//Select the one checkbox and save it in a variable called checkbox.
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Read the problem at app.js where there will be a small excercise.
+Then check if you have done it correctly or not.
